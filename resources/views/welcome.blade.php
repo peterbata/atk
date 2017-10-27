e2136b64f4fcffa645ae0d1079bb9fc63cb24d1b
peterbata <peterbata@hotmail.com>
2017-10-26 20:10:19 -0400

    Second commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel 5.5
+                    Laravel Rocks
                 </div>
 
                 <div class="links">
